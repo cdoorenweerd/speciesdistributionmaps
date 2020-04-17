03b7b30f09699a74f5a651a65cd5072dd925d2a4
Camiel Doorenweerd <c.doorenweerd@gmail.com>
2020-04-16 14:28:26 -1000

    fixed value error

--- speciesmap_maker.py
+++ speciesmap_maker.py
@@ -37,21 +37,24 @@ def simplemap(species):
        dfhistsp = dfhist.query('taxon == @species')
        col_y_hist = list(dfhistsp['latitude'])
        col_x_hist = list(dfhistsp['longitude'])
-       m.plot(col_x_hist, col_y_hist, marker='^',color='#f1a340', markersize=3, zorder=27, latlon=True,
+       col_x_hist,col_y_hist = m(col_x_hist,col_y_hist)
+       m.plot(col_x_hist, col_y_hist, marker='^',color='#f1a340', markersize=3, zorder=27, latlon=False,
               linestyle='None', markeredgewidth=.2, markeredgecolor='black', alpha=.8)
     
        # plot surplus data
        dfsurplussp = dfsurplus.query('taxon == @species')
        col_y_surplus = list(dfsurplussp['latitude'])
        col_x_surplus = list(dfsurplussp['longitude'])
-       m.plot(col_x_surplus, col_y_surplus, marker='.',color='#998ec3', markersize=7, zorder=28, latlon=True,
+       col_x_surplus,col_y_surplus = m(col_x_surplus,col_y_surplus)
+       m.plot(col_x_surplus, col_y_surplus, marker='.',color='#998ec3', markersize=7, zorder=28, latlon=False,
            linestyle='None', markeredgewidth=.2, markeredgecolor='black', alpha=.8)
 
        # plot ms sample data
        dfmscodessp = dfmscodes.query('final_id == @species')
        col_y_mscodes = list(dfmscodessp['latitude'])
        col_x_mscodes = list(dfmscodessp['longitude'])
-       m.plot(col_x_mscodes, col_y_mscodes, marker='.',color='#2ca25f', markersize=7, zorder=29, latlon=True,
+       col_x_mscodes,col_y_mscodes = m(col_x_mscodes,col_y_mscodes)
+       m.plot(col_x_mscodes, col_y_mscodes, marker='.',color='#2ca25f', markersize=7, zorder=29, latlon=False,
            linestyle='None', markeredgewidth=.2, markeredgecolor='black', alpha=.8)
 
        # save to plate
@@ -75,7 +78,7 @@ conn = None
 specieslist = dfmscodes['final_id'].unique().tolist()
 Path("./output_maps").mkdir(parents=True, exist_ok=True)
 
-print("Making maps for " + len(specieslist) + "species")
+print("Making maps for " + str(len(specieslist)) + " species")
 for species in specieslist:
        try:
               simplemap(species)
